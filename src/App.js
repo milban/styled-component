cd6ef739c6ee2ebf542ab04a8ce5c323f7968e2f
JunHyeock Park <wnsgur6311@gmail.com>
2019-08-16 13:01:15 +0900

    Mixin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import styled, { createGlobalStyle, css, keyframes } from "styled-components"
+import styled, { createGlobalStyle, css } from "styled-components"
 
 const GlobalStyel = createGlobalStyle`
   body{
@@ -8,11 +8,20 @@ const GlobalStyel = createGlobalStyle`
   }
 `
 
+const awesomeCard = css`
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px,
+    rgba(0, 0, 0, 0.024) 0px -6px 16px -6px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`
+
 const Input = styled.input.attrs({
   required: true
 })`
   border: none;
   border-radius: 5px;
+  ${awesomeCard}
 `
 
 class App extends Component {
